c962a5791769afd6145fbd6e378567be00837291
ishitashita <iishita038@gmail.com>
2025-05-17 22:37:15 +0530

    Time: 11 ms (10.98%) | Memory: 46 MB (5.30%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
@@ -8,22 +8,32 @@
  * }
  */
 class Solution {
+    public TreeNode compare(ArrayList<TreeNode> l1, ArrayList<TreeNode> l2){
+        int i =0;
+        while(i<l1.size() && i<l2.size() && l1.get(i)==l2.get(i)){
+            i++;
+        }
+        return (i > 0) ? l1.get(i - 1) : null;
+    }
+    public boolean fillList(ArrayList<TreeNode> l1, TreeNode root, TreeNode find){
+        if(root == null)return false;
+        l1.add(root);
+        if(root == find) return true;
+        if(fillList(l1,root.left, find)|| fillList(l1, root.right, find))return true;
+        l1.remove(l1.size()-1);
+        return false;
+        
+
+    }
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        // Base case
-        if (root == null || root == p || root == q) {
-            return root;
-        }
-        
-        // Recur for left and right subtrees
-        TreeNode left = lowestCommonAncestor(root.left, p, q);
-        TreeNode right = lowestCommonAncestor(root.right, p, q);
-        
-        // If both left and right are non-null, this is the LCA
-        if (left != null && right != null) {
-            return root;
-        }
-        
-        // Otherwise, return non-null child (either left or right)
-        return left != null ? left : right;
+       ArrayList<TreeNode> path1 = new ArrayList<>();
+       ArrayList<TreeNode> path2 = new ArrayList<>();
+
+       fillList(path1, root, p);
+       fillList(path2, root, q);
+       return compare(path1, path2);
+
+     
+     
     }
 }
