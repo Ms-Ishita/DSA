f3fe381f5928002e8097ec62e2a0bd5c482eaacd
ishitashita <iishita038@gmail.com>
2025-06-16 13:13:36 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (92.67%) - LeetSync

--- 92-reverse-linked-list-ii/reverse-linked-list-ii.java
+++ 92-reverse-linked-list-ii/reverse-linked-list-ii.java
@@ -9,49 +9,43 @@
  * }
  */
 class Solution {
-    
-public ListNode reverseBetween(ListNode head, int l, int r) {
-    //left pointer
-    //right pointer 
-    if(l==r||head==null)return head;
-    ListNode dummy = new ListNode(0);
-    dummy.next = head;
-    head=dummy;
-    ListNode leftprev = head;    
-    ListNode rightnext = head;
-    for(int i =0; i<l-1; i++){
-       leftprev = leftprev.next;
+    public ListNode reverseBetween(ListNode head, int left, int right) { 
+           
+        if(left==right || head == null)return head;
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        head = dummy ;
+        ListNode leftprev = head;
+        ListNode rightnext = head;
+        for(int i =0; i<left-1; i++){
+            leftprev = leftprev.next;
+        }
+        ListNode l= leftprev.next;
+        ListNode r = head;
+        
+        for(int i =0; i<right; i++){
+            r= r.next;
+        }
+        rightnext=r.next;
+        
+        leftprev.next = null;
+        r.next = null;
+        
+        ListNode prev = null;
+        ListNode curr = l;
+        ListNode next = null;
+        
+        while(curr!=null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr= next;
+        }
+        leftprev.next = r;
+        l.next = rightnext;
+        
+        return dummy.next;
+        
+        
     }
-    ListNode left= leftprev.next;
-    ListNode right=head;
-    for(int i =0; i<r; i++){
-        right = right.next;
-    }
-    rightnext=right.next;
-    
-    leftprev.next=null;
-    right.next=null;
-    
-
-    ListNode prev = null;
-    ListNode curr = left;
-    ListNode next = null;
-
-    while(curr!=null){
-        next = curr.next;
-        curr.next = prev;
-        prev= curr;
-        curr = next;  
-    }
-    
-    leftprev.next=right;
-    left.next=rightnext;
-
-    return dummy.next;
-
-
-    //reverse the middle linked list
-    
-    //arrangement 
-}
 }
