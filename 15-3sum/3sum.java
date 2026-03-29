59dc75e2af240cb22d68e07b5933b69117390ae0
Ishita <iishita038@gmail.com>
2026-03-29 17:56:33 +0530

    Time: 883 ms (8.43%) | Memory: 59.8 MB (20.85%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,32 +1,27 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-     Set<List<Integer>> s = new HashSet<>();
-     if(nums.length==0)return new ArrayList();
+        Set<List<Integer>> set = new HashSet<>();
+        List<Integer> list = new ArrayList<>();
         Arrays.sort(nums);
-     int sum = 0;
-     for(int i = 0; i<nums.length-2; i++){
-        int left = i+1;
-        int right = nums.length-1;
-
-        while(left<right){
-            sum = nums[i]+nums[left]+nums[right];
-            if(sum==0){
-                s.add(Arrays.asList(nums[i], nums[left], nums[right]));
-                left++;
-                right--;
-
+        for(int k=0; k<nums.length -2; k++){
+            int i =k+1; 
+            int j =nums.length-1;
+            while(i<j){
+                int sum = nums[k] + nums[i] + nums[j];
+                if(sum == 0){
+                    set.add(Arrays.asList(nums[k], nums[i], nums[j]));
+                    i++;
+                    j--;
+                }
+                else if (sum>0)j--;
+                else i++;
             }
-            else if(sum<0){
-                left++;
-            }
-            else{
-                right--;
-            }
-             
             
-        }
-     }
-     return new ArrayList(s);
 
+        }
+        return new ArrayList<>(set);
+
+
+        
     }
 }
