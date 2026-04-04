81021dad25812ea43e53c937baf85388fb33a4ed
Ishita <iishita038@gmail.com>
2026-04-04 21:14:44 +0530

    Time: 28 ms (27.36%) | Memory: 47.2 MB (9.76%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,23 +1,39 @@
 class Solution {
+    public boolean checkMapIsEmpty(Map<Character, Integer> map){
+        for(char key: map.keySet()){
+            if(map.get(key)!=0)return false;
+        }
+        return true;
+    }
     public boolean checkInclusion(String s1, String s2) {
-        char[] ch1 = s1.toCharArray();
-        Arrays.sort(ch1);
-        String newStr1 = new String(ch1);
-        int s1Len = newStr1.length();
-        int left =0;
+
+        Map<Character, Integer> map = new HashMap<>();
+        int s1Len = s1.length();
+        int s2Len = s2.length();
+        for(int i =0; i<s1Len; i++){
+            char ch = s1.charAt(i);
+            if(!map.containsKey(ch))map.put(ch, 1);
+            else map.put(ch, map.get(ch)+1);
+        }
+        int left = 0;
         int right = 0;
-        while(right<s2.length()){
-            if(right-left+1 == s1Len){
-                String str2 = s2.substring(left, right + 1);
-                char[] ch2 = str2.toCharArray();
-                Arrays.sort(ch2);
-                String newStr2 = new String(ch2);
-                if(newStr2.equals(newStr1))return true;
+        while(right<s2Len){
+            
+            if(map.containsKey(s2.charAt(right))){
+                map.put(s2.charAt(right), map.get(s2.charAt(right))-1);
+
+            }
+            if((right-left)+1> s1Len){
+                if(map.containsKey(s2.charAt(left))){
+                    map.put(s2.charAt(left), map.get(s2.charAt(left))+1);
+                }
                 left++;
             }
+            if((right-left)+1 == s1Len && checkMapIsEmpty(map))return true;
+             right++;
 
-            right++;
         }
+       
         return false;
     }
 }
