e58cea991c75f0bb248f84c800c2dee6052c2c60
Ishita <iishita038@gmail.com>
2026-04-16 01:00:07 +0530

    Time: 17 ms (19.93%) | Memory: 47.2 MB (95.77%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,29 +1,61 @@
-class Solution {
-    public int[] topKFrequent(int[] nums, int k) {
-        Map<Integer, Integer> map = new HashMap<>();
-        int[] arr = new int[k];
-        for(int i =0; i<nums.length; i++){
-            if(!map.containsKey(nums[i]))map.put(nums[i], 1);
-            else map.put(nums[i], map.get(nums[i])+1);
+// class Solution {
+//     public int[] topKFrequent(int[] nums, int k) {
+//         Map<Integer, Integer> map = new HashMap<>();
+//         int[] arr = new int[k];
+//         for(int i =0; i<nums.length; i++){
+//             if(!map.containsKey(nums[i]))map.put(nums[i], 1);
+//             else map.put(nums[i], map.get(nums[i])+1);
         
-        }
+//         }
         
-        for(int i =0; i<k; i++){
-            int max = -1;
-            int ele = -1;
-            for(int key: map.keySet()){
-                if(map.get(key)> max){
-                    max= map.get(key);
-                    ele = key;
-                }
+//         for(int i =0; i<k; i++){
+//             int max = -1;
+//             int ele = -1;
+//             for(int key: map.keySet()){
+//                 if(map.get(key)> max){
+//                     max= map.get(key);
+//                     ele = key;
+//                 }
                 
 
-            }
-            arr[i]= ele;
-            map.remove(ele);
+//             }
+//             arr[i]= ele;
+//             map.remove(ele);
 
-        }
-        return arr;
+//         }
+//         return arr;
         
+//     }
+// }
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        // max heap based on frequency
+        PriorityQueue<Integer> pq =
+                new PriorityQueue<>((a,b) -> map.get(b) - map.get(a));
+
+        int[] arr = new int[k];
+
+        // count frequency
+        for(int i = 0; i < nums.length; i++){
+            if(!map.containsKey(nums[i]))
+                map.put(nums[i], 1);
+            else
+                map.put(nums[i], map.get(nums[i]) + 1);
+        }
+
+        // add ELEMENTS (not frequency)
+        for(int key : map.keySet()){
+            pq.add(key);
+        }
+
+        // take top k elements
+        for(int i = 0; i < k; i++){
+            arr[i] = pq.poll();
+        }
+
+        return arr;
     }
 }
