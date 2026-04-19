ebbff216468752cc8b68addd862878896d9546a6
Ishita <iishita038@gmail.com>
2026-04-19 15:19:28 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (61.80%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
@@ -10,20 +10,28 @@
  */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        ListNode dummy = new ListNode(0);
+        ListNode dummy = new ListNode(-1);
         dummy.next = head;
         ListNode fast = dummy;
         ListNode slow = dummy;
-        for(int i =0; i<n; i++){
-            fast = fast.next;    
+
+        for(int i =0;i<n; i++){
+            fast = fast.next;
         }
         while(fast!=null && fast.next!=null){
-            fast= fast.next;
+            fast = fast.next;
             slow = slow.next;
 
         }
         slow.next = slow.next.next;
         return dummy.next;
         
+       
+
+        
+
+
+        
+
     }
 }
