970e91986efde3ee65b6bf8b140e61c85297af83
Ishita <iishita038@gmail.com>
2026-05-04 14:09:47 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (67.23%) - LeetSync

---
+++ 3625-stone-removal-game/stone-removal-game.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean canAliceWin(int n) {
+       int remove = 10;
+       while(n>=remove){
+        n-=remove;
+        remove--;
+        
+       }
+       return (remove%2!=0);
+        
+        
+    }
+}
