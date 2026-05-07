b56616665c07ceac58cf7c230602e404ab93c646
Ishita <iishita038@gmail.com>
2026-05-07 18:28:24 +0530

    Time: 2 ms (29.77%) | Memory: 45.3 MB (65.59%) - LeetSync

--- 443-string-compression/string-compression.java
+++ 443-string-compression/string-compression.java
@@ -3,22 +3,23 @@ class Solution {
         StringBuilder sb = new StringBuilder();
         int left = 0;
         int right = 0;
-        while(right<=chars.length){
-          
-            if(right==chars.length || right<chars.length && chars[left]!=chars[right]){
+        while(right<chars.length){
+            if(chars[left]!=chars[right]){
                 sb.append(chars[left]);
                 if(right-left>1){
                     sb.append(right-left);
                 }
-                
                 left = right;
             } 
             right++;
         }
+        sb.append(chars[left]);
+                if(right-left>1){
+                    sb.append(right-left);
+                }
         for(int i =0; i<sb.length(); i++){
             chars[i] = sb.charAt(i);
         }
         return sb.length();
-        
     }
 }
