6214dcf9a31332b7602be5dac0a28a746aff926d
Ishita <iishita038@gmail.com>
2026-05-09 02:53:03 +0530

    Time: 152 ms (5.08%) | Memory: 162.9 MB (5.86%) - LeetSync

---
+++ 2107-find-unique-binary-string/find-unique-binary-string.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Solution {
+
+    public void backTracking(String curr, int n, List<String> list) {
+
+        if (curr.length() == n) {
+            list.add(curr);
+            return;
+        }
+
+        backTracking(curr + "0", n, list);
+        backTracking(curr + "1", n, list);
+    }
+
+    public String findDifferentBinaryString(String[] nums) {
+
+        List<String> list = new ArrayList<>();
+
+        backTracking("", nums[0].length(), list);
+
+        Set<String> set = new HashSet<>(Arrays.asList(nums));
+
+        for (String s : list) {
+            if (!set.contains(s)) {
+                return s;
+            }
+        }
+
+        return "";
+    }
+}
