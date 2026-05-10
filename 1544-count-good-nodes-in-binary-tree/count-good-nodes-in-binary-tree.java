260d74919bce5f737425d55362c122707e4c705e
Ishita <iishita038@gmail.com>
2026-05-10 20:06:47 +0530

    Time: 2 ms (94.41%) | Memory: 56.8 MB (81.34%) - LeetSync

--- 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.java
+++ 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.java
@@ -4,7 +4,7 @@
  *     int val;
  *     TreeNode left;
  *     TreeNode right;
- *     TreeNode()/{}
+ *     TreeNode() {}
  *     TreeNode(int val) { this.val = val; }
  *     TreeNode(int val, TreeNode left, TreeNode right) {
  *         this.val = val;
@@ -14,31 +14,18 @@
  * }
  */
 class Solution {
-    public int goodNodes(TreeNode root) {
-        return isgoodNodes(root,Integer.MIN_VALUE);
+    public void countGoodNodes(TreeNode root,int max, int[] count){
+        if(root == null)return ;
+        if(max<=root.val)count[0]+=1;
+        countGoodNodes(root.left, (max>root.val)?max:root.val, count);
+        countGoodNodes(root.right,(max>root.val)?max:root.val, count);
+
     }
-    public int isgoodNodes(TreeNode root, int max){
-        //soch balak jo jo comment mei likha h         
-        if(root == null)return 0;
+    public int goodNodes(TreeNode root) {
+        int[] count = new int[2];
+        countGoodNodes(root, Integer.MIN_VALUE, count);
+        return count[0];
+
         
-        // think that this return 0 defines what
-        int count=0;
-        if(root.val>=max){
-            
-            max=root.val;
-            count++;
-            
-        // return 1;
-        }
-        
-        int left=isgoodNodes(root.left,max);
-        int right=isgoodNodes(root.right,max);
-        return count+left+right;
-        // think what happens you find out that what happens when you find out it is greatest in the path 
-
-        // think how would you handle the count as you are returning 0 when you reach null think about it 
-
-        //what you return at end its a big question 
-
     }
 }
