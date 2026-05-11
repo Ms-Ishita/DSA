fa1c080691b4d97d1458150ddc2860efebcfcddb
Ishita <iishita038@gmail.com>
2026-05-11 21:15:45 +0530

    Time: 3 ms (98.10%) | Memory: 62.9 MB (88.25%) - LeetSync

--- 9-palindrome-number/palindrome-number.js
+++ 9-palindrome-number/palindrome-number.js
@@ -4,7 +4,13 @@
  */
 var isPalindrome = function(x) {
     let str = x.toString();
-    let reversed = str.split("").reverse().join("");
-    return reversed=== str;
+    let left = 0;
+    let right = str.length-1;
+    while(left<right){
+        if(str[left]!=str[right])return false;
+        left++;
+        right--;
+    }
+    return true;
     
 };
