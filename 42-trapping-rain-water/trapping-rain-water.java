f3cdd7794696f805dcf84d07bf2b364475aed6d3
Ishita <iishita038@gmail.com>
2026-05-15 02:34:51 +0530

    Time: 1 ms (58.95%) | Memory: 48.6 MB (22.17%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -7,12 +7,12 @@ class Solution {
       for(int i =1; i<height.length; i++){
         left[i] = Math.max(left[i-1], height[i]);
       }
-      System.out.println(Arrays.toString(left));
+     
       right[height.length-1] = height[height.length-1];
       for(int i =height.length-2; i>=0;i--){
         right[i] =Math.max( right[i+1],height[i]);
       }
-      System.out.println(Arrays.toString(right));
+   
       int result = 0;
       for(int i =0; i<height.length; i++){
         result +=Math.min(left[i],right[i])-height[i];
