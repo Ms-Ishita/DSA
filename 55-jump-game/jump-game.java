f3e912d00e5b37610b781e452292d17277ef0322
Ishita <iishita038@gmail.com>
2026-05-20 09:43:05 +0530

    Time: 1 ms (99.99%) | Memory: 47.9 MB (60.67%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,22 +1,39 @@
 class Solution {
-    public boolean jump(int[] nums, int i, Boolean[] dp){
-        
-        if(i>=nums.length-1)return true;
-        if(nums[i]==0)return false;
-        if(dp[i]!=null)return dp[i];
-        for(int steps =1; steps<=nums[i]; steps++){
-            if(jump(nums, i+steps, dp)){
-                dp[i] = true;
-                return true ;
-            }
-        }
-        dp[i] = false;
-        return false;
-
-    }
     public boolean canJump(int[] nums) {
-        Boolean[] dp = new Boolean[nums.length];
-        return jump(nums, 0, dp);
+        int final_position = nums.length-1;
+        for(int i =nums.length-2; i>=0; i--){
+            if(i+nums[i]>=final_position){
+                final_position = i;
+            }
+            
+        }
+        return final_position ==0;
+        
+        
+       
+       
         
     }
 }
+// class Solution {
+//     public boolean jump(int[] nums, int i, Boolean[] dp){
+        
+//         if(i>=nums.length-1)return true;
+//         if(nums[i]==0)return false;
+//         if(dp[i]!=null)return dp[i];
+//         for(int steps =1; steps<=nums[i]; steps++){
+//             if(jump(nums, i+steps, dp)){
+//                 dp[i] = true;
+//                 return true ;
+//             }
+//         }
+//         dp[i] = false;
+//         return false;
+
+//     }
+//     public boolean canJump(int[] nums) {
+//         Boolean[] dp = new Boolean[nums.length];
+//         return jump(nums, 0, dp);
+        
+//     }
+// }
