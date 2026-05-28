6376a599bfd2ef6c4e67b2d7fbc675e9b9649da2
Ishita <iishita038@gmail.com>
2026-05-28 11:25:57 +0530

    Time: 48 ms (24.73%) | Memory: 47.9 MB (11.90%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -1,29 +1,38 @@
 class Solution {
+    public boolean checkMap(HashMap<Character,Integer> map){
+        for(char ch : map.keySet()){
+            if(map.get(ch)!=0)return false;
+        }
+        return true;
+    }
     public List<Integer> findAnagrams(String s, String p) {
-        List<Integer> list = new ArrayList<>();
-        if(s.length()<p.length())return new ArrayList<>();
-        int pLen = p.length();
-        int sLen = s.length();
-        char[] ch1 = p.toCharArray();
-        Arrays.sort(ch1);
-        String oldStr = new String(ch1);
+        List<Integer> ans = new ArrayList<>();
+        HashMap<Character, Integer> map = new HashMap<>();
+        for(int i =0; i<p.length(); i++){
+            if(!map.containsKey(p.charAt(i)))map.put(p.charAt(i),1);
+            else map.put(p.charAt(i), map.get(p.charAt(i))+1);
+        }
         int left =0;
-        int right =0;
-        while(right<sLen){
-            if((right-left)+1 == pLen){
-                String str2 = s.substring(left, right+1);
-                char[] ch2 = str2.toCharArray();
-                Arrays.sort(ch2);
-                String newStr = new String(ch2);
-                if(newStr.equals(oldStr)){
-                    list.add(left);
+        int len = p.length();
+        for(int right =0; right<s.length(); right++){
+            char ch = s.charAt(right);
+            if(map.containsKey(ch))map.put(ch, map.get(ch)-1);
+            if(right-left+1 >len){
+                char leftChar = s.charAt(left);
+                if(map.containsKey(leftChar)){
+                    map.put(leftChar, map.get(leftChar)+1);
                 }
                 left++;
             }
+            if(right-left+1 == len){
+                if(checkMap(map)){
+                    ans.add(left);
+                }
+                
 
-            right++;
+            }
         }
-        return list;
+        return ans ;
 
     }
 }
