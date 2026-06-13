41d8566135a4efe9ed0bb0808dfb075c9051cd7b
Ishita <iishita038@gmail.com>
2026-06-13 14:30:00 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (55.20%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,15 +1,14 @@
 class Solution {
-    public int robbering(int[] nums, int i, int[] dp){
+    public int robbering(int[] nums,int i, int[] dp){
         if(i>=nums.length)return 0;
         if(dp[i]!=-1)return dp[i];
-        int exclude = robbering(nums, i+1, dp);
-        int include = nums[i]+robbering(nums, i+2, dp);
-        return dp[i] = Math.max(exclude, include);
+        int skip = robbering(nums, i+1, dp);
+        int take = nums[i]+robbering(nums,i+2, dp);
+        return dp[i] = Math.max(skip, take); 
     }
     public int rob(int[] nums) {
-        int[] dp = new int[nums.length];
+        int[] dp = new int[nums.length+1];
         Arrays.fill(dp,-1);
         return robbering(nums, 0, dp);
-        
     }
 }
