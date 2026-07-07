0d0c694601ef657b0146435a05f7f6d2348b8770
Ishita <iishita038@gmail.com>
2026-07-07 06:15:16 +0530

    Time: 28 ms (5.10%) | Memory: 47.4 MB (5.04%) - LeetSync

--- 684-redundant-connection/redundant-connection.java
+++ 684-redundant-connection/redundant-connection.java
@@ -1,30 +1,32 @@
 class Solution {
-    // public int find()
-    public boolean dfs(ArrayList<ArrayList<Integer>> adj, boolean[] visited, int src, int dest){
-        if(src==dest)return true;
-        if(visited[src])return false;
+    public boolean isPath(List<List<Integer>> adj,int src,int dest, boolean[] visited){
+        if(src == dest)return true;
         visited[src] = true;
         for(int i =0; i<adj.get(src).size(); i++){
             int next = adj.get(src).get(i);
-            if(dfs(adj, visited, next, dest))return true;
+            if(!visited[next]){
+                if(isPath(adj,next,dest,visited))return true;
+            }
         }
         return false;
     }
     public int[] findRedundantConnection(int[][] edges) {
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        for(int i =0;i<=edges.length; i++){
+        List<List<Integer>> adj = new ArrayList<>();
+        for(int i =0; i<edges.length+1; i++){
             adj.add(new ArrayList<>());
         }
         for(int i =0; i<edges.length; i++){
-            boolean[] visited = new boolean[edges.length+2];
-            
-            int v = edges[i][0];
-            int u = edges[i][1];
-            if(dfs(adj,visited,v,u))return new int[]{v,u};
-            adj.get(v).add(u);
+            int u = edges[i][0];
+            int v = edges[i][1];
+            boolean[] visited = new boolean[edges.length+1];
+            if(isPath(adj,u,v,visited)){
+                return new int[]{u,v};
+            }
             adj.get(u).add(v);
+            adj.get(v).add(u);
         }
         return new int[]{};
+      
         
     }
 }
