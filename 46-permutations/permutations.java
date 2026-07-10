5f7667a97f5affec6e65f277d2a99fd148d830ca
Ishita <iishita038@gmail.com>
2026-07-10 18:50:16 +0530

    Time: 1 ms (87.32%) | Memory: 45.6 MB (46.54%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,25 +1,26 @@
 class Solution {
-    public  void backtraking(int[] nums, List<List<Integer>> list, List<Integer> result, int i, boolean[] visited ){
-        if(i==nums.length){
-            list.add(new ArrayList<>(result));
+    public void backtracking(int[] nums,boolean[] visited, List<Integer>list, List<List<Integer>>result){
+        if(list.size()==nums.length){
+            result.add(new ArrayList<>(list));
             return;
         }
-        for(int j =0; j<nums.length; j++){
-            if(!visited[j]){
-            visited[j] = true;
-            result.add(nums[j]);
-            backtraking(nums,list,result,i+1,visited);
-            result.remove(result.size()-1);
-            visited[j] = false;
+        for(int i =0; i<nums.length; i++){
+            if(!visited[i]){
+                visited[i]=true;
+                list.add(nums[i]);
+                backtracking(nums,visited,list,result);
+                list.remove(list.size()-1);
+                visited[i] = false;
             }
         }
+  
     }
-    public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> list = new ArrayList<>();
-        List<Integer> result = new ArrayList<>();
+    public List<List<Integer>> permute(int[] nums){
+        List<Integer> list = new ArrayList<>();
+        List<List<Integer>> result = new ArrayList<>();
         boolean[] visited = new boolean[nums.length];
-        backtraking(nums,list,result,0,visited);
-        return list;
+        backtracking(nums,visited,list,result);
+        return result;
         
     }
 }
