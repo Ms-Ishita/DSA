bd3bd8f67b17b0e3851a8efda9302ad6d8dd57e4
Ishita <iishita038@gmail.com>
2026-09-11 18:55:54 +0530

    Time: 27 ms (24.73%) | Memory: 47 MB (10.74%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,39 +1,34 @@
 class Solution {
-    public boolean checkMapIsEmpty(Map<Character, Integer> map){
-        for(char key: map.keySet()){
+    public boolean checkMap(Map<Character, Integer> map ){
+          for(char key: map.keySet()){
             if(map.get(key)!=0)return false;
         }
         return true;
+
     }
     public boolean checkInclusion(String s1, String s2) {
-
         Map<Character, Integer> map = new HashMap<>();
-        int s1Len = s1.length();
-        int s2Len = s2.length();
-        for(int i =0; i<s1Len; i++){
+        for(int i =0; i<s1.length(); i++){
             char ch = s1.charAt(i);
             if(!map.containsKey(ch))map.put(ch, 1);
             else map.put(ch, map.get(ch)+1);
         }
-        int left = 0;
-        int right = 0;
-        while(right<s2Len){
-            
-            if(map.containsKey(s2.charAt(right))){
-                map.put(s2.charAt(right), map.get(s2.charAt(right))-1);
-
-            }
-            if((right-left)+1> s1Len){
-                if(map.containsKey(s2.charAt(left))){
-                    map.put(s2.charAt(left), map.get(s2.charAt(left))+1);
-                }
+        int left =0;
+        int s1_len = s1.length();
+        for(int right =0; right<s2.length(); right++){
+            char ch = s2.charAt(right);
+            if(map.containsKey(ch))map.put(ch,map.get(ch)-1);
+            if(right-left+1>s1_len){
+                if(map.containsKey(s2.charAt(left)))map.put(s2.charAt(left), map.get(s2.charAt(left))+1);
                 left++;
             }
-            if((right-left)+1 == s1Len && checkMapIsEmpty(map))return true;
-             right++;
+            if(right-left+1 == s1_len && checkMap(map))return true;
+                
+            
+
 
         }
-       
         return false;
+        
     }
 }
