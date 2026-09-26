68389322b08ca92a51f311d3bb147b4cad573e79
Ishita <iishita038@gmail.com>
2026-09-26 18:42:19 +0530

    Time: 3 ms (32.91%) | Memory: 45.7 MB (67.07%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,23 +1,19 @@
 class Solution {
-    public  void comSum(int[] candidates, int target , List<Integer> list1, List<List<Integer>> list2, int i){
-        if(target ==0){
-            list2.add(new ArrayList<>(list1));
+    public void backTraking(int[] candidates, int target, int i, List<Integer> ans, List<List<Integer>> result){
+        if(target==0){
+            result.add(new ArrayList<>(ans));
             return;
         }
-        if(i== candidates.length || target<0) return;
-
-        list1.add(candidates[i]);
-        comSum(candidates, target - candidates[i], list1, list2, i);
-        list1.remove(list1.size()-1);
-        comSum(candidates, target, list1, list2, i+1);
-
-
+        if(i == candidates.length || target<0) return;
+        ans.add(candidates[i]);
+        backTraking(candidates,target-candidates[i],i,ans,result);
+        ans.remove(ans.size()-1);
+        backTraking(candidates,target,i+1,ans, result);
     }
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<Integer> list1 = new ArrayList<>();
-        List<List<Integer>> list2 = new ArrayList<>();
-        comSum(candidates, target, list1, list2, 0);
-        return list2;
-        
+        List<Integer> ans = new ArrayList<>();
+        List<List<Integer>> result = new ArrayList<>();
+        backTraking(candidates,target,0,ans,result);
+        return result;
     }
 }
